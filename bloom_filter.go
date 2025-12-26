84f0f9aef9fa9edce93dc8b5f139341308f60c68
Stefan Milojevic <stefanmiloje87@gmail.com>
2025-12-26 17:14:54 +0100

    [feat] Implementirao NewBloomFilter, IsFound i Set funkcije za Bloom Filter

--- bloom_filter.go
+++ bloom_filter.go
@@ -3,8 +3,8 @@ package main
 import (
 	"crypto/md5"
 	"encoding/binary"
-	"math"
 	"errors"
+	"math"
 	"time"
 )
 
@@ -13,27 +13,51 @@ type HashWithSeed struct {
 }
 
 type bloomFilter struct {
-	// TODO: Reconsider the name
-	bitset []byte
-	hashFunctions []HashWithSeed
-	m uint
-	k uint
+	bitset    []byte
+	hashFuncs []HashWithSeed
+	m         uint
+	k         uint64
 }
 
-func (bf bloomFilter) IsFound(data []byte) (bool, error) {
-	return false, errors.New("not implemented")
+var masks = [8]byte{
+	1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7,
 }
 
-func (bf bloomFilter) Set(data []byte) error {
-	return errors.New("not implemented")
+func (bf bloomFilter) IsFound(data []byte) bool {
+	for _, hashFunc := range bf.hashFuncs {
+		i := (hashFunc.hash(data)) % bf.k
+		target := bf.bitset[i/8]
+		if target&masks[i%8] == 0 {
+			return false
+		}
+	}
+	return true
 }
 
-func NewBloomFilter(expectedElements int, falsePositiveRate float64) (bloomFilter, error) {
-	// TODO: check params
-	return bloomFilter{}, errors.New("not implemented")
+func (bf bloomFilter) Set(data []byte) {
+	for _, hashFunc := range bf.hashFuncs {
+		i := (hashFunc.hash(data)) % bf.k
+		target := bf.bitset[i/8]
+		bf.bitset[i/8] = target | masks[i%8]
+	}
 }
 
-func DumpBloomFilter(bf bloomFilter, filename string) (error) {
+func NewBloomFilter(expectedElements uint, falsePositiveRate float64) (*bloomFilter, error) {
+	if falsePositiveRate <= 0 || falsePositiveRate >= 1 {
+		return nil, errors.New("falsePositiveRate must be in range (0, 1)")
+	}
+	m := calculateM(expectedElements, falsePositiveRate)
+	k := calculateK(expectedElements, m)
+	bitset := make([]byte, k/8)
+	return &bloomFilter{
+		bitset,
+		createHashFunctions(k),
+		m,
+		k,
+	}, nil
+}
+
+func DumpBloomFilter(bf bloomFilter, filename string) error {
 	return errors.New("not implemented")
 }
 
@@ -41,25 +65,12 @@ func LoadBloomFilter(filename string) (bloomFilter, error) {
 	return bloomFilter{}, errors.New("not implemented")
 }
 
-// PERF: Save the masks maybe
-func (bf bloomFilter) setBit(i uint) {
-	target := bf.bitset[i/8]
-	var mask byte = 1 << (i % 8)
-	bf.bitset[i/8] = target | mask
-}
-
-func (bf bloomFilter) getBit(i uint) bool {
-	target := bf.bitset[i/8]
-	var mask byte = 1 << (i % 8)
-	return (target & mask) != 0
-}
-
-func calculateM(expectedElements int, falsePositiveRate float64) uint {
+func calculateM(expectedElements uint, falsePositiveRate float64) uint {
 	return uint(math.Ceil(float64(expectedElements) * math.Abs(math.Log(falsePositiveRate)) / math.Pow(math.Log(2), float64(2))))
 }
 
-func calculateK(expectedElements int, m uint) uint {
-	return uint(math.Ceil((float64(m) / float64(expectedElements)) * math.Log(2)))
+func calculateK(expectedElements uint, m uint) uint64 {
+	return uint64(math.Ceil((float64(m) / float64(expectedElements)) * math.Log(2)))
 }
 
 func (h HashWithSeed) hash(data []byte) uint64 {
@@ -68,12 +79,12 @@ func (h HashWithSeed) hash(data []byte) uint64 {
 	return binary.BigEndian.Uint64(fn.Sum(nil))
 }
 
-func createHashFunctions(k uint32) []HashWithSeed {
+func createHashFunctions(k uint64) []HashWithSeed {
 	h := make([]HashWithSeed, k)
-	ts := uint32(time.Now().Unix())
+	ts := uint64(time.Now().Unix())
 	for i := range k {
 		seed := make([]byte, 4)
-		binary.BigEndian.PutUint32(seed, ts+i)
+		binary.BigEndian.PutUint64(seed, ts+i)
 		hfn := HashWithSeed{Seed: seed}
 		h[i] = hfn
 	}
