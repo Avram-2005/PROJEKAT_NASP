3ec548a5c0d22eca3cb6b5f2045ad0f6c5de7324
Tanja Teofanovic <tanjateofanovic321@gmail.com>
2025-12-28 19:53:26 +0100

    Implementirano clear i estimate za CMS

--- countMinSketch.go
+++ countMinSketch.go
@@ -3,73 +3,108 @@ package projekatnasp
 import (
 	"crypto/md5"
 	"encoding/binary"
+	"fmt"
 	"math"
 	"time"
 )
 
-type CountMinSketch struct {
-	k     uint    // broj hash funkcija(redovi)
-	m     uint    // broj kolona
-	table [][]int // k x m matrica
+type countMinSketch struct {
+	k      uint           // broj hash funkcija(redovi)
+	m      uint           // broj kolona
+	table  [][]int        // k x m matrica
+	hashes []hashWithSeed // hash funkcije
 }
 
 // kreiranje nove instance
-func NewCountMinSketch(epsilon, delta float64) *CountMinSketch {
-	m := CalculateM(epsilon)
-	k := CalculateK(delta)
+func NewCountMinSketch(epsilon, delta float64) (*countMinSketch, error) {
+	if epsilon <= 0 || epsilon > 1 {
+		return nil, fmt.Errorf("Epsilon must be lesser than 1 and greater than 0")
+	}
+	if delta <= 0 || delta > 1 {
+		return nil, fmt.Errorf("Delta must be lesser than 1 and greater than 0")
+	}
+
+	m := calculateM(epsilon)
+	k := calculateK(delta)
 
 	table := make([][]int, k)
 	for i := range table {
 		table[i] = make([]int, m)
 	}
 
-	return &CountMinSketch{
-		k:     k,
-		m:     m,
-		table: table,
-	}
+	return &countMinSketch{
+		k:      k,
+		m:      m,
+		table:  table,
+		hashes: createHashFunctions(k),
+	}, nil
 }
 
-func (cms *CountMinSketch) Add(key string) {
+func (cms *countMinSketch) Add(key string) {
 	// pretvaranje kljuca u niz bajtova
 	data := []byte(key)
 
-	// kreiranje k hash funkcija
-	hashes := CreateHashFunctions(cms.k)
+	for i := uint(0); i < cms.k; i++ {
+		j := cms.hashes[i].hash(data) % uint64(cms.m)
+		cms.table[i][j]++
+	}
+}
+
+func (cms *countMinSketch) Estimate(key string) int {
+	data := []byte(key)
+	r := make([]int, cms.k) // niz duzine k
 
 	for i := uint(0); i < cms.k; i++ {
-		j := hashes[i].Hash(data) % uint64(cms.m)
-		cms.table[i][j]++
+		j := cms.hashes[i].hash(data) % uint64(cms.m)
+		r[i] = cms.table[i][j]
+	}
+
+	// trazi minimum
+	min := r[0]
+	for i := 1; i < len(r); i++ {
+		if r[i] < min {
+			min = r[i]
+		}
+	}
+
+	return min
+}
+
+func (cms *countMinSketch) Clear() {
+	for i := uint(0); i < cms.k; i++ {
+		for j := uint(0); j < cms.m; j++ {
+			cms.table[i][j] = 0
+		}
 	}
 }
 
 // helper functions
 
-func CalculateM(epsilon float64) uint {
+func calculateM(epsilon float64) uint {
 	return uint(math.Ceil(math.E / epsilon))
 }
 
-func CalculateK(delta float64) uint {
+func calculateK(delta float64) uint {
 	return uint(math.Ceil(math.Log(math.E / delta)))
 }
 
-type HashWithSeed struct {
+type hashWithSeed struct {
 	Seed []byte
 }
 
-func (h HashWithSeed) Hash(data []byte) uint64 {
+func (h hashWithSeed) hash(data []byte) uint64 {
 	fn := md5.New()
 	fn.Write(append(data, h.Seed...))
 	return binary.BigEndian.Uint64(fn.Sum(nil))
 }
 
-func CreateHashFunctions(k uint) []HashWithSeed {
-	h := make([]HashWithSeed, k)
+func createHashFunctions(k uint) []hashWithSeed {
+	h := make([]hashWithSeed, k)
 	ts := uint(time.Now().Unix())
 	for i := uint(0); i < k; i++ {
 		seed := make([]byte, 32)
 		binary.BigEndian.PutUint32(seed, uint32(ts+i))
-		hfn := HashWithSeed{Seed: seed}
+		hfn := hashWithSeed{Seed: seed}
 		h[i] = hfn
 	}
 	return h
