ac0ed8ae79a78776f4889cca0465de0e1c3d78af
Aki-man <aleksaperic.skola@gmail.com>
2026-01-06 23:59:32 +0100

    [test][feat]Dodati testovi za bufferpool i block manager, i uradjena mala prepravka. Hristos se rodi, srecan bozic

--- BlockManager/BlockManager.go
+++ BlockManager/BlockManager.go
@@ -19,7 +19,7 @@ type BlockManager struct {
 // Iz tog razloga, jedine dozvoljene vrednosti za blockSize su 4, 8 i 16, ako stavite ista drugo dobicete error.
 // Konverzija iz kilobajta u bajtove desava se unutar konstruktora za BufferPool, vi se oko toga ne brinete.
 func NewBlockManager(maxSize int, blockSize int) (*BlockManager, error) {
-	if maxSize != 4 && maxSize != 8 && maxSize != 16 {
+	if blockSize != 4 && blockSize != 8 && blockSize != 16 {
 		return nil, fmt.Errorf("dozvoljene vrednosti za blockSize su 4, 8, ili 16")
 	}
 	cache, err := BufferPool.NewBufferPool(maxSize, blockSize)
@@ -34,6 +34,7 @@ func NewBlockManager(maxSize int, blockSize int) (*BlockManager, error) {
 // filepath-path do fajla iz kojeg se dobavljaju informacije
 // blockNumber-broj bloka koji se trazi
 // funkcija vraca niz bajtova, i error u slucaju da je nesto poslo po zlu
+// BITNO: funkcija vam vraca CEO SADRZAJ BLOKA, na vama je da nadjete sta vam treba unutar njega
 func (bm *BlockManager) Get(filepath string, blockNumber int) (*[]byte, error) {
 	valueFound, err := bm.blockCache.Get(filepath, blockNumber)
 	if err != nil {
@@ -46,6 +47,8 @@ func (bm *BlockManager) Get(filepath string, blockNumber int) (*[]byte, error) {
 // blockNumber-broj bloka koji se trazi
 // writeValue-niz bajtova koji treba da se upise na trazen blok
 // funkcija vraca samo error, u slucaju da je nesto poslo po zlu
+// BITNO: kada upisujete nesto u fajl ovom metodom, CEO SADRZAJ BLOKA se override-uje
+// na vam je da unutar vasih struktura regulisete kako se vasi zapisi dele po blokovima.
 func (bm *BlockManager) Put(filepath string, blockNumber int, writeValue *[]byte) error {
 	err := bm.blockCache.Put(filepath, blockNumber, writeValue)
 	return err
