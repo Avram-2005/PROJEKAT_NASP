64edc492af9b9359fbe49d275372984caf206c09
Aki-man <aleksaperic.skola@gmail.com>
2026-01-06 22:49:09 +0100

    [fix]BufferPool zapravo funkcionise sad

--- BufferPool/BufferPool_test.go
+++ BufferPool/BufferPool_test.go
@@ -3,29 +3,20 @@ package BufferPool
 import (
 	"encoding/binary"
 	"fmt"
-	"os"
+	"reflect"
 	"testing"
 )
 
 func TestBufferPool(t *testing.T) {
 	data1 := make([]byte, 4096)
 	binary.BigEndian.PutUint64(data1, 78)
-	fmt.Print(data1)
 	data2 := make([]byte, 4096)
-	binary.BigEndian.PutUint32(data1, 56)
-	fmt.Print(data2)
+	binary.BigEndian.PutUint32(data2, 56)
 	data3 := make([]byte, 4096)
-	binary.BigEndian.PutUint16(data1, 67)
-	fmt.Print(data3)
+	binary.BigEndian.PutUint16(data3, 67)
 	bp, err := NewBufferPool(3, 4)
 	if err != nil {
 		t.Errorf("greska tokom inicalizacije bufferpoola")
-		t.FailNow()
-	}
-	err = os.WriteFile("test.bin", data1, 0644)
-	if err != nil {
-		fmt.Print(err)
-		t.Errorf("greska tokom nultog pisanja")
 		t.FailNow()
 	}
 
@@ -36,16 +27,61 @@ func TestBufferPool(t *testing.T) {
 		t.Errorf("greska tokom prvog pisanja")
 		t.FailNow()
 	}
+
+	readData1, err := bp.Get("test.bin", 1)
+	fmt.Print(readData1)
+	if err != nil {
+		fmt.Print(err)
+		t.Errorf("greska tokom prvog citanja")
+		t.FailNow()
+	}
+
+	if !reflect.DeepEqual(data1, (*readData1)) {
+		fmt.Print(data1[:10])
+		fmt.Print((*readData1)[:10])
+		t.Errorf("data1 nije isto pre i posle citanja")
+		t.FailNow()
+	}
+
 	err = bp.Put("test.bin", 2, &data2)
 	if err != nil {
 		fmt.Print(err)
 		t.Errorf("greska tokom drugog pisanja")
 		t.FailNow()
 	}
+
+	readData2, err := bp.Get("test.bin", 2)
+	if err != nil {
+		fmt.Print(err)
+		t.Errorf("greska tokom drugog citanja")
+		t.FailNow()
+	}
+
+	if !reflect.DeepEqual(data2, (*readData2)) {
+		fmt.Print(data2[:10])
+		fmt.Print((*readData2)[:10])
+		t.Errorf("data2 nije isto pre i posle citanja")
+		t.FailNow()
+	}
+
 	err = bp.Put("test.bin", 3, &data3)
 	if err != nil {
 		fmt.Print(err)
 		t.Errorf("greska tokom treceg pisanja")
+		t.FailNow()
+	}
+
+	readData3, err := bp.Get("test.bin", 3)
+	if err != nil {
+		fmt.Print(err)
+		t.Errorf("greska tokom treceg citanja")
+		t.FailNow()
+	}
+
+	if !reflect.DeepEqual(data3, (*readData3)) {
+		fmt.Print(data3[:10])
+		fmt.Print((*readData3)[:10])
+		t.Errorf("data3 nije isto pre i posle citanja")
 		t.FailNow()
 	}
 
