ee01eac39b32ca336f56ab079c71b57f320d506a
Aki-man <aleksaperic.skola@gmail.com>
2026-04-01 16:29:13 +0200

    [Snapshot][feat][refactor][test] Uradjen refactor koda da bi snapshot pratio logicku vrednost umesto fizicke, zajedno sa testovima za dodatu funkcionalnost

--- Snapshot/Snapshot.go
+++ Snapshot/Snapshot.go
@@ -1,100 +1,53 @@
-package Snapshot
+package snapshot
 
 import (
-	"container/list"
-	"fmt"
+	"os"
+	"time"
+
+	"github.com/Avram-2005/PROJEKAT_NASP/BlockManager"
 )
 
-// Snapshot je mapa koja na svakom kljucu sadrži listu verzija vrednosti na nekom ključu
-// ovaj struct zapravo predstavlja mapu koja skladisti snapshot-ove raznih kljuceva baze
 type Snapshot struct {
-	snapshotMap map[string]*list.List
+	filepath    string
+	blockNumber int
+	offset      int
+	size        int
+	timestamp   time.Time
 }
 
-// Konstruktor ne zahteva ikakve parametre-snapshot objekat stvaramo pri pokretanju,
-// pa ga punimo sa kljucevima koje korisnik zeli da prati
-// TODO: proveriti da li mozda trebaju neki parametri
-func NewSnapshot() (*Snapshot, error) {
-	newMap := make(map[string]*list.List)
+func NewSnapshot(filepath string, blockNumber int, offset int, size int, timestamp time.Time, bm *BlockManager.BlockManager) (*Snapshot, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	_, err = bm.GetSpecific(file, blockNumber, offset, size)
+	file.Close()
+	if err != nil {
+		return nil, err
+	}
 	return &Snapshot{
-		snapshotMap: newMap,
+		filepath:    filepath,
+		blockNumber: blockNumber,
+		offset:      offset,
+		size:        size,
+		timestamp:   timestamp,
 	}, nil
 }
 
-// Funkcija koja ili stvara novi snapshot za odredjen kljuc,
-// ili dodaje novu vrednost u postojeci
-// key-kljuc pod koji dodajemo
-// value-niz bajtova, najnovija vrednost
-func (sp *Snapshot) Add(key string, value *[]byte) error {
-	foundList, ok := sp.snapshotMap[key]
-	// ako kljuc nije pronadjen, stvaramo nov snapshot
-	if !ok {
-		newList := list.New()
-		newList.PushFront(value)
-		sp.snapshotMap[key] = newList
-		return nil
-	}
-	// ako kljuc vec postoji, dodajemo novu vrednost na kraj njegovog snapshot-a
-	foundList.PushFront(value)
-	return nil
+func (sp *Snapshot) GetTimestamp() time.Time {
+	return sp.timestamp
 }
 
-// Funkcija koja dobavlja odredjenu verziju naseg podatka
-// key-kljuc koji se trazi
-// version-verzija kljuca koju trazimo, gde je prva dodata vrednost nulta verzija,
-// i svaka naredna je veca za jedan
-func (sp *Snapshot) Get(key string, version int) (*[]byte, error) {
-	foundList, ok := sp.snapshotMap[key]
-	//error ako kljuc koji trazimo ne postoji
-	if !ok || foundList.Len() == 0 {
-		return nil, fmt.Errorf("key not found in snapshot")
-	}
-	counter := 0
-	for elem := foundList.Back(); elem != nil; elem = elem.Prev() {
-		// da bi pronasli odgovarajucu verziju koristimo brojac i petlju
-		if counter == version {
-			return elem.Value.(*[]byte), nil
-		}
-		counter += 1
-	}
-	return nil, fmt.Errorf("version number not found for the specified key")
-}
+func (sp *Snapshot) GetValue(bm *BlockManager.BlockManager) (*[]byte, error) {
+	file, err := os.Open(sp.filepath)
+	if err != nil {
 
-// Funkcija koja dobavlja nultu verziju nase vrednosti
-// key-kljuc pod kojim se trazi
-func (sp *Snapshot) GetFirst(key string) (*[]byte, error) {
-	foundList, ok := sp.snapshotMap[key]
-	if !ok || foundList.Len() == 0 {
-		return nil, fmt.Errorf("key not found in snapshot")
+		return nil, err
 	}
-	return foundList.Back().Value.(*[]byte), nil
-}
-
-// Funkcija dobavlja poslednju verziju nase vrednosti
-// key-kljuc pod kojim trazimo
-func (sp *Snapshot) GetLatest(key string) (*[]byte, error) {
-	foundList, ok := sp.snapshotMap[key]
-	if !ok || foundList.Len() == 0 {
-		return nil, fmt.Errorf("key not found in snapshot")
+	returnValue, err := bm.GetSpecific(file, sp.blockNumber, sp.offset, sp.size)
+	file.Close()
+	if err != nil {
+		return nil, err
 	}
-	return foundList.Front().Value.(*[]byte), nil
-}
-
-// Funkcija dobavlja broj razlicitih verzija koje se skladiste za odredjen kljuc
-func (sp *Snapshot) GetVersionCount(key string) (int, error) {
-	foundList, ok := sp.snapshotMap[key]
-	if !ok {
-		return 0, fmt.Errorf("key is not tracked in this snapshot")
-	}
-	return foundList.Len(), nil
-}
-
-// Izbacuje snapshot za specifican kljuc iz radne memorije
-func (sp *Snapshot) Free(key string) error {
-	_, ok := sp.snapshotMap[key]
-	if !ok {
-		return fmt.Errorf("cannot free the memory of a nonexistent snapshot")
-	}
-	delete(sp.snapshotMap, key)
-	return nil
+	return returnValue, nil
 }
---
+++ SnapshotManager/SnapshotManager.go
@@ -0,0 +1,185 @@
+package SnapshotManager
+
+import (
+	"container/list"
+	"fmt"
+	"time"
+
+	"github.com/Avram-2005/PROJEKAT_NASP/BlockManager"
+	snapshot "github.com/Avram-2005/PROJEKAT_NASP/Snapshot"
+)
+
+// SnapshotManager je mapa koja na svakom kljucu sadrži listu verzija vrednosti na nekom ključu
+// ovaj struct zapravo predstavlja mapu koja skladisti SnapshotManager-ove raznih kljuceva baze
+type SnapshotManager struct {
+	SnapshotManagerMap map[string]*list.List
+}
+
+// Konstruktor ne zahteva ikakve parametre-SnapshotManager objekat stvaramo pri pokretanju,
+// pa ga punimo sa kljucevima koje korisnik zeli da prati
+// TODO: proveriti da li mozda trebaju neki parametri
+func NewSnapshotManager() (*SnapshotManager, error) {
+	newMap := make(map[string]*list.List)
+	return &SnapshotManager{
+		SnapshotManagerMap: newMap,
+	}, nil
+}
+
+// Funkcija koja ili stvara novi SnapshotManager za odredjen kljuc,
+// ili dodaje novu vrednost u postojeci
+// key-kljuc pod koji dodajemo
+// value-niz bajtova, najnovija vrednost
+func (sp *SnapshotManager) Add(key string, value *snapshot.Snapshot) error {
+	foundList, ok := sp.SnapshotManagerMap[key]
+	// ako kljuc nije pronadjen, stvaramo nov snapshot
+	if !ok {
+		newList := list.New()
+		newList.PushFront(value)
+		sp.SnapshotManagerMap[key] = newList
+		return nil
+	}
+	// ako kljuc vec postoji, dodajemo novu vrednost na kraj njegovog SnapshotManager-a
+	foundList.PushFront(value)
+	return nil
+}
+
+// Funkcija koja dobavlja odredjenu verziju naseg podatka
+// key-kljuc koji se trazi
+// version-verzija kljuca koju trazimo, gde je prva dodata vrednost nulta verzija,
+// i svaka naredna je veca za jedan
+func (sp *SnapshotManager) Get(key string, version int) (*snapshot.Snapshot, error) {
+	foundList, ok := sp.SnapshotManagerMap[key]
+	//error ako kljuc koji trazimo ne postoji
+	if !ok || foundList.Len() == 0 {
+		return nil, fmt.Errorf("key not found in SnapshotManager")
+	}
+	counter := 0
+	for elem := foundList.Back(); elem != nil; elem = elem.Prev() {
+		// da bi pronasli odgovarajucu verziju koristimo brojac i petlju
+		if counter == version {
+			return elem.Value.(*snapshot.Snapshot), nil
+		}
+		counter += 1
+	}
+	return nil, fmt.Errorf("version number not found for the specified key")
+}
+
+// Funkcija koja dobavlja odredjenu verziju naseg podatka
+// key-kljuc koji se trazi
+// timestamp-timestamp verzije koju trazimo
+func (sp *SnapshotManager) GetByTimestamp(key string, timestamp time.Time) (*snapshot.Snapshot, error) {
+	foundList, ok := sp.SnapshotManagerMap[key]
+	//error ako kljuc koji trazimo ne postoji
+	if !ok || foundList.Len() == 0 {
+		return nil, fmt.Errorf("key not found in SnapshotManager")
+	}
+	for elem := foundList.Back(); elem != nil; elem = elem.Prev() {
+		// da bi pronasli odgovarajucu verziju koristimo brojac i petlju
+		snapshot := elem.Value.(*snapshot.Snapshot)
+		if snapshot.GetTimestamp().Equal(timestamp) {
+			return snapshot, nil
+		}
+	}
+	return nil, fmt.Errorf("version number not found for the specified key")
+}
+
+// Funkcija koja dobavlja vrednost specificne verzije snapshot-a
+// key-kljuc
+// version-verzija(po redu dodavanja)
+// bm-blockmanager koji ce izvuci podatke sa diska
+func (sp *SnapshotManager) GetValue(key string, version int, bm *BlockManager.BlockManager) (*[]byte, error) {
+	snapshot, err := sp.Get(key, version)
+	if err != nil {
+		return nil, err
+	}
+	value, err := snapshot.GetValue(bm)
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
+
+// Funkcija koja dobavlja vrednost specificne verzije snapshot-a
+// key-kljuc
+// timestamp-timestamp podatka koji trazimo
+// bm-blockmanager koji ce izvuci podatke sa diska
+func (sp *SnapshotManager) GetValueByTimestamp(key string, timestamp time.Time, bm *BlockManager.BlockManager) (*[]byte, error) {
+	snapshot, err := sp.GetByTimestamp(key, timestamp)
+	if err != nil {
+		return nil, err
+	}
+	value, err := snapshot.GetValue(bm)
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
+
+// Funkcija koja dobavlja nultu verziju nase vrednosti
+// key-kljuc pod kojim se trazi
+func (sp *SnapshotManager) GetFirst(key string) (*snapshot.Snapshot, error) {
+	foundList, ok := sp.SnapshotManagerMap[key]
+	if !ok || foundList.Len() == 0 {
+		return nil, fmt.Errorf("key not found in SnapshotManager")
+	}
+	return foundList.Back().Value.(*snapshot.Snapshot), nil
+}
+
+// Funkcija dobavlja poslednju verziju nase vrednosti
+// key-kljuc pod kojim trazimo
+func (sp *SnapshotManager) GetLatest(key string) (*snapshot.Snapshot, error) {
+	foundList, ok := sp.SnapshotManagerMap[key]
+	if !ok || foundList.Len() == 0 {
+		return nil, fmt.Errorf("key not found in SnapshotManager")
+	}
+	return foundList.Front().Value.(*snapshot.Snapshot), nil
+}
+
+// Funkcija dobavlja prvu vrednost odredjenog kljuca
+// key-kljuc
+// bm-blockmanager koji ce izvuci podatke sa diska
+func (sp *SnapshotManager) GetValueFirst(key string, bm *BlockManager.BlockManager) (*[]byte, error) {
+	snapshot, err := sp.GetFirst(key)
+	if err != nil {
+		return nil, err
+	}
+	value, err := snapshot.GetValue(bm)
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
+
+// Funkcija dobavlja poslednju vrednost odredjenog kljuca
+// key-kljuc
+// bm-blockmanager koji ce izvuci podatke sa diska
+func (sp *SnapshotManager) GetValueLatest(key string, bm *BlockManager.BlockManager) (*[]byte, error) {
+	snapshot, err := sp.GetLatest(key)
+	if err != nil {
+		return nil, err
+	}
+	value, err := snapshot.GetValue(bm)
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
+
+// Funkcija dobavlja broj razlicitih verzija koje se skladiste za odredjen kljuc
+func (sp *SnapshotManager) GetVersionCount(key string) (int, error) {
+	foundList, ok := sp.SnapshotManagerMap[key]
+	if !ok {
+		return 0, fmt.Errorf("key is not tracked in this SnapshotManager")
+	}
+	return foundList.Len(), nil
+}
+
+// Izbacuje SnapshotManager za specifican kljuc iz radne memorije
+func (sp *SnapshotManager) Free(key string) error {
+	_, ok := sp.SnapshotManagerMap[key]
+	if !ok {
+		return fmt.Errorf("cannot free the memory of a nonexistent SnapshotManager")
+	}
+	delete(sp.SnapshotManagerMap, key)
+	return nil
+}
