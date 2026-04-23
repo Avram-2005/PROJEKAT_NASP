c9986162a1368ac77138a1c96b77695330cc6b55
Aki-man <aleksaperic.skola@gmail.com>
2026-04-23 23:10:30 +0200

    Add BufferPool Config function

--- Config/Config.go
+++ Config/Config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"bytes"
+	"os"
+
+	"github.com/Avram-2005/PROJEKAT_NASP/BlockManager"
+	"go.yaml.in/yaml/v4"
+)
+
+type Config struct {
+	BufferPoolConfig struct {
+		MaxSize   int `yaml:"MaxSize"`
+		BlockSize int `yaml:"BlockSize"`
+	} `yaml:"BufferPoolConfig"`
+}
+
+func NewConfig() *Config {
+	return &Config{}
+}
+
+func (config *Config) Initialize(bm *BlockManager.BlockManager, configFile *os.File) error {
+	data, err := bm.Get(configFile, 0)
+	if err != nil {
+		return err
+	}
+	cleanedData := bytes.TrimRight(*data, "\x00")
+	err = yaml.Unmarshal(cleanedData, config)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- Config/Config_test.go
+++ Config/Config_test.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/Avram-2005/PROJEKAT_NASP/BlockManager"
+	"go.yaml.in/yaml/v4"
+)
+
+func TestInitialize(t *testing.T) {
+	bm, err := BlockManager.NewBlockManager(4, 4)
+	if err != nil {
+		fmt.Print(err)
+		t.FailNow()
+	}
+	file, err := os.OpenFile("config_test.yaml", 0644, os.FileMode(os.O_RDONLY))
+	if err != nil {
+		fmt.Print(err)
+		t.FailNow()
+	}
+	config := NewConfig()
+	err = config.Initialize(bm, file)
+	if err != nil {
+		fmt.Print(err)
+		t.FailNow()
+	}
+
+	config.BufferPoolConfig.BlockSize = 4
+	config.BufferPoolConfig.MaxSize = 4
+	data, err := yaml.Marshal(config)
+	if err != nil {
+		fmt.Print(err)
+		t.FailNow()
+	}
+	fmt.Print(string(data))
+}
