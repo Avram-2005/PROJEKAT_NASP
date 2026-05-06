d9447c5d88381143b9dd7c087356223d4764d4cd
Stefan Milojevic <stefanmilojevic87@gmail.com>
2026-05-06 23:16:18 +0200

    [test] Wrote initEngine

--- Engine/Engine_test.go
+++ Engine/Engine_test.go
@@ -1,1 +1,20 @@
 package engine
+
+import "testing"
+
+func initEngine(t *testing.T) Engine {
+	root := t.TempDir()
+	configPath := root + "/config.json"
+	walPath := root + "/wal"
+	sstablePath := root + "/sstables"
+
+	engine, err := NewEngine(configPath, walPath, sstablePath)
+	if err != nil {
+		t.Fatalf("Failed to initialize engine: %v", err)
+	}
+	return *engine
+}
+
+func TestEngineOnePutGet(t *testing.T) {
+
+}
