6464eeaae6d99662423b4199507b7fb299666990
AlexJ192 <aleksandraj192@gmail.com>
2026-05-08 00:05:46 +0200

    [feat] Dodate funkcije za prefix i range scan koje obuhvataju sve intsance memtable-a

--- Memtable/memtableManager.go
+++ Memtable/memtableManager.go
@@ -155,3 +155,20 @@ func (mm *MemtableManager) TotalSize() int {
 	}
 	return total
 }
+
+// skenira sve instance memtable-a za zadati prefix
+func (mm *MemtableManager) PrefixScan(prefix string) []*record.Record {
+	result := make([]*record.Record, 0)
+	for _, instance := range mm.instances {
+		result = append(result, instance.PrefixScan(prefix)...)
+	}
+	return result
+}
+
+func (mm *MemtableManager) RangeScan(startKey, endKey string) []*record.Record {
+	result := make([]*record.Record, 0)
+	for _, instance := range mm.instances {
+		result = append(result, instance.RangeScan(startKey, endKey)...)
+	}
+	return result
+}
