78130839dadaa8fb4991613301d47116d3900711
AlexJ192 <aleksandraj192@gmail.com>
2026-05-08 19:14:39 +0200

    [feat]Dodate pomocne funkcije za iteratore

--- Scan/system_iterator.go
+++ Scan/system_iterator.go
@@ -33,3 +33,46 @@ func (s *SystemScanner) NewSystemRangeIterator(startKey, endKey string) (*System
 		stopped: false,
 	}, nil
 }
+
+//pomera iterator na sledeci zapis
+func (it *SystemIterator) Next() bool {
+	if it.stopped || it.index+1 >= len(it.records) {
+		return false
+	}
+	it.index++
+	return true
+}
+
+//vraca trenutni kljuc
+func (it *SystemIterator) Key() string {
+	if it.index < 0 || it.index >= len(it.records) {
+		return ""
+	}
+	return it.records[it.index].Key
+}
+
+//vraca trenutnu vrednost
+func (it *SystemIterator) Value() []byte {
+	if it.index < 0 || it.index >= len(it.records) {
+		return nil
+	}
+	return it.records[it.index].Value
+}
+
+func (it *SystemIterator) Tombstone() bool {
+	return false //uvek vraca false jer su filtrirani
+}
+
+//zaustavlja iterator i oslobadja resurse
+func (it *SystemIterator) Stop() {
+	it.stopped = true
+	it.records = nil
+	it.index = -1
+}
+
+//vraca iterator na poocetak
+func (it *SystemIterator) Reset() {
+	if !it.stopped {
+		it.index = -1
+	}
+}
