cebc682214155a7fb999368b973d96eaea0f2732
AlexJ192 <aleksandraj192@gmail.com>
2026-05-08 21:26:06 +0200

    [feat] Dodat range iterate u CLI

--- main.go
+++ main.go
@@ -215,7 +215,45 @@ func mainMenu() {
 			fmt.Println("----------------------------------------------")
 
 		case 7:
-			fmt.Println("RANGE_ITERATE is not implemented!")
+			startKey := readLine("Enter start key: ")
+			endKey := readLine("Enter endKey: ")
+			if startKey == "" || endKey == "" {
+				fmt.Println("Start and end keys must not be empty.")
+				continue
+			}
+			iter, err := engine.NewRangeIterator(startKey, endKey)
+			if err != nil {
+				fmt.Printf("Failed to create iterator: %v\n", err)
+				continue
+			}
+			defer iter.Stop()
+			fmt.Println("Iterating through records: ")
+			fmt.Println("----------------------------------------------")
+			count := 0
+			for {
+				fmt.Print("\nPress 'n' for next record, 's' to stop: ")
+				var command string
+				fmt.Scanln(&command)
+				if command == "s" || command == "stop" {
+					iter.Stop()
+					fmt.Println("Iteration stopped.")
+					break
+				}
+				if command == "n" || command == "next" {
+					if !iter.Next() {
+						fmt.Println("No more records.End of iteration.")
+						break
+					}
+					count++
+					fmt.Printf("%d. Key: %s, Value: %s\n", count, iter.Key(), string(iter.Value()))
+				} else {
+					fmt.Println("Unknown command. Use 'n' for next, 's' to stop.")
+				}
+			}
+			if count == 0 {
+				fmt.Println("No results found.")
+			}
+			fmt.Println("----------------------------------------------")
 
 		case 8:
 			fmt.Println("SNAPSHOT is not implemented!")
