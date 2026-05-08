ba88bbeb6d78be34532ce4d16ace933d41a10342
AlexJ192 <aleksandraj192@gmail.com>
2026-05-08 15:39:36 +0200

    [feat] Dodat prefix scan nad celim sistemom uz paginaciju

--- Scan/scan.go
+++ Scan/scan.go
@@ -37,10 +37,9 @@ func (s *SystemScanner) RangeScan(startKey, endKey string, pageNumber, pageSize 
 	}
 	//sakupljanje records iz svih instanci iz memtable-a
 	allRecords := make([]*record.Record, 0)
-	//for i := 0; i < s.memtable.InstanceCount(); i++ {
 	records := s.memtable.RangeScan(startKey, endKey)
 	allRecords = append(allRecords, records...)
-	//}
+
 	//sakupljanje iz sstable-a
 	sstabRecords, err := s.lsm.RangeScan(startKey, endKey)
 	if err != nil {
@@ -49,29 +48,29 @@ func (s *SystemScanner) RangeScan(startKey, endKey string, pageNumber, pageSize 
 	for i := range sstabRecords {
 		allRecords = append(allRecords, sstabRecords[i])
 	}
-
+	//sortiranje po kljucu
 	sort.Slice(allRecords, func(i, j int) bool {
 		return allRecords[i].Key < allRecords[j].Key
 	})
-
+	//zadrzavanje novijeg timestampa i uklanjanje duplikata (tombstone)
 	unique := make(map[string]*record.Record)
 	for _, rec := range allRecords {
 		if exists, ok := unique[rec.Key]; !ok || rec.Timestamp.After(exists.Timestamp) {
 			unique[rec.Key] = rec
 		}
 	}
-
+	//filtriranje tombstone-a
 	activeRecords := make([]*record.Record, 0)
 	for _, rec := range unique {
 		if !rec.Tombstone {
 			activeRecords = append(activeRecords, rec)
 		}
 	}
-
+	//sortiranje po kljucu
 	sort.Slice(activeRecords, func(i, j int) bool {
 		return activeRecords[i].Key < activeRecords[j].Key
 	})
-
+	//paginacija
 	startIndex := (pageNumber - 1) * pageSize
 	if startIndex >= len(activeRecords) {
 		return &ScanResult{
@@ -87,6 +86,73 @@ func (s *SystemScanner) RangeScan(startKey, endKey string, pageNumber, pageSize 
 	if endIndex > len(activeRecords) {
 		endIndex = len(activeRecords)
 	}
+	return &ScanResult{
+		Records:    activeRecords[startIndex:endIndex],
+		TotalCount: len(activeRecords),
+		PageNumber: pageNumber,
+		PageSize:   pageSize,
+		HasMore:    endIndex < len(activeRecords),
+	}, nil
+}
+
+func (s *SystemScanner) PrefixScan(prefix string, pageNumber, pageSize int) (*ScanResult, error) {
+	if pageNumber < 1 {
+		pageNumber = 1
+	}
+	if pageSize < 1 {
+		pageSize = 10
+	}
+	allRecords := make([]*record.Record, 0)
+	records := s.memtable.PrefixScan(prefix) //sakupljanje records iz svih instanci iz memtable-a
+	allRecords = append(allRecords, records...)
+
+	sstabRecords, err := s.lsm.PrefixScan(prefix) //sakupljanje svih records iz sstable-a
+	if err != nil {
+		return nil, err
+	}
+	for i := range sstabRecords {
+		allRecords = append(allRecords, sstabRecords[i])
+	}
+
+	sort.Slice(allRecords, func(i, j int) bool { //sortiranje po kljucu
+		return allRecords[i].Key < allRecords[j].Key
+	})
+
+	unique := make(map[string]*record.Record)
+	for _, rec := range allRecords { //uklanjanje duplikata i zadrzavanje novijeg timestampa
+		if exist, ok := unique[rec.Key]; !ok || rec.Timestamp.After(exist.Timestamp) {
+			unique[rec.Key] = rec
+		}
+	}
+
+	activeRecords := make([]*record.Record, 0)
+	for _, rec := range unique { //filtriranje tombsone-a
+		if !rec.Tombstone {
+			activeRecords = append(activeRecords, rec)
+		}
+	}
+
+	sort.Slice(allRecords, func(i, j int) bool { //sortiranje po kljucu
+		return allRecords[i].Key < allRecords[j].Key
+	})
+
+	//paginacija
+	startIndex := (pageNumber - 1) * pageSize
+	if startIndex >= len(activeRecords) {
+		return &ScanResult{
+			Records:    []*record.Record{},
+			TotalCount: len(activeRecords),
+			PageNumber: pageNumber,
+			PageSize:   pageSize,
+			HasMore:    false,
+		}, nil
+	}
+
+	endIndex := startIndex + pageSize
+	if endIndex > len(activeRecords) {
+		endIndex = len(activeRecords)
+	}
+
 	return &ScanResult{
 		Records:    activeRecords[startIndex:endIndex],
 		TotalCount: len(activeRecords),
