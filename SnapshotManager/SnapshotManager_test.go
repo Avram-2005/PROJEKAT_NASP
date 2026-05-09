0666270e3cf1cb4bde75d801f3f6c5ddfe201613
Aki-man <aleksaperic.skola@gmail.com>
2026-05-09 04:13:52 +0200

    Delete incomplete test

--- SnapshotManager/SnapshotManager_test.go
+++ SnapshotManager/SnapshotManager_test.go
@@ -9,73 +9,7 @@ import (
 	"testing"
 
 	engine "github.com/Avram-2005/PROJEKAT_NASP/Engine"
-	sstable "github.com/Avram-2005/PROJEKAT_NASP/LSM"
-	memtable "github.com/Avram-2005/PROJEKAT_NASP/Memtable"
-	record "github.com/Avram-2005/PROJEKAT_NASP/Record"
 )
-
-func TestSnapshotManager(t *testing.T) {
-	sp, err := NewSnapshotManager()
-	if err != nil {
-		fmt.Print(err)
-		t.Errorf("greska tokom incijalizacije SnapshotManager-a")
-		t.FailNow()
-	}
-	config := memtable.MemtableConfig{
-		Type:              "hashmap",
-		MaxSizeBytes:      200,
-		MaxSizeEntries:    10,
-		SkipListMaxHeight: 8,
-		BPlusTreeDegree:   2,
-	}
-	memtableManager, err := memtable.NewMemtableManager(5, config, func(entries []*record.Record) error {
-		return nil
-	})
-	if err != nil {
-		fmt.Print("error initializing memtable")
-		t.FailNow()
-	}
-
-	for i := 0; i < 3; i++ {
-		temp := make([]byte, 10)
-		random := uint32(rand.Intn(100))
-		binary.BigEndian.PutUint32(temp, random)
-		key := "key" + strconv.Itoa(i)
-		memtableManager.Put(key, temp)
-	}
-	for i := 0; i < 3; i++ {
-		temp := make([]byte, 10)
-		random := uint32(rand.Intn(100))
-		binary.BigEndian.PutUint32(temp, random)
-		key := "key" + strconv.Itoa(i)
-		memtableManager.Put(key, temp)
-	}
-	memtables := memtableManager.GetMemtables()
-
-	sstables := make([]*sstable.SSTable, 3)
-	sstableManager := &sstable.SSTableManager{}
-	sp.AddMany("key0", &memtables, &sstables, sstableManager)
-	sp.AddMany("key1", &memtables, &sstables, sstableManager)
-	sp.AddMany("key2", &memtables, &sstables, sstableManager)
-	versions, err := sp.GetVersionCount("key0")
-	if err != nil {
-		fmt.Print(err)
-		t.FailNow()
-	}
-	fmt.Print(versions)
-	versions, err = sp.GetVersionCount("key1")
-	if err != nil {
-		fmt.Print(err)
-		t.FailNow()
-	}
-	fmt.Print(versions)
-	versions, err = sp.GetVersionCount("key2")
-	if err != nil {
-		fmt.Print(err)
-		t.FailNow()
-	}
-	fmt.Print(versions)
-}
 
 func TestSnapshotWithEngine(t *testing.T) {
 	sp, err := NewSnapshotManager()
